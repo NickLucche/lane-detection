fixed scaling bug; added some utility methods for visual evaluation

hough_transform.py
@@ -12,9 +12,9 @@ def do_canny(frame):
     canny = cv.Canny(blur, 50, 150)
     return canny
 
+
 def do_segment(frame):
-    # Since an image is a multi-directional array containing the relative intensities of each pixel in the image, we can use frame.shape to return a tuple: [number of rows, number of columns, number of channels] of the dimensions of the frame
-    # frame.shape[0] give us the number of rows of pixels the frame has. Since height begins from 0 at the top, the y-coordinate of the bottom of the frame is its height
+
     height = frame.shape[0]
     # Creates a triangular polygon for the mask defined by three (x, y) coordinates
     polygons = np.array([
@@ -27,6 +27,7 @@ def do_segment(frame):
     # A bitwise and operation between the mask and frame keeps only the triangular area of the frame
     segment = cv.bitwise_and(frame, mask)
     return segment
+
 
 def calculate_lines(frame, lines):
     # Empty arrays to store the coordinates of the left and right lines
@@ -75,29 +76,3 @@ def visualize_lines(frame, lines):
             cv.line(lines_visualize, (x1, y1), (x2, y2), (0, 255, 0), 5)
     return lines_visualize
 
-# The video feed is read in as a VideoCapture object
-cap = cv.VideoCapture("input.mp4")
-while (cap.isOpened()):
-    # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
-    ret, frame = cap.read()
-    canny = do_canny(frame)
-    cv.imshow("canny", canny)
-    # plt.imshow(frame)
-    # plt.show()
-    segment = do_segment(canny)
-    hough = cv.HoughLinesP(segment, 2, np.pi / 180, 100, np.array([]), minLineLength = 100, maxLineGap = 50)
-    # Averages multiple detected lines from hough into one line for left border of lane and one line for right border of lane
-    lines = calculate_lines(frame, hough)
-    # Visualizes the lines
-    lines_visualize = visualize_lines(frame, lines)
-    cv.imshow("hough", lines_visualize)
-    # Overlays lines on frame by taking their weighted sums and adding an arbitrary scalar value of 1 as the gamma argument
-    output = cv.addWeighted(frame, 0.9, lines_visualize, 1, 1)
-    # Opens a new window and displays the output frame
-    cv.imshow("output", output)
-    # Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
-    if cv.waitKey(10) & 0xFF == ord('q'):
-        break
-# The following frees up resources and closes all windows
-cap.release()
-cv.destroyAllWindows()

test.py
@@ -16,8 +16,6 @@ import argparse
 import time
 
 
-
-
 def validate(val_loader, model, criterion, log_every=1):
     batch_time = AverageMeter('Time', ':6.3f')
     losses = AverageMeter('Loss', ':.4e')
@@ -30,7 +28,7 @@ def validate(val_loader, model, criterion, log_every=1):
         [batch_time, losses, acc, f1, prec, rec],
         prefix='Test: ')
 
-    # switch to evaluate mode
+    # evaluate mode highly decreases performance
     model.train()
 
     correct = 0
@@ -49,7 +47,12 @@ def validate(val_loader, model, criterion, log_every=1):
             # compute loss
             loss = criterion(output, targets)
             losses.update(loss.item(), targets.size(0))
-            print(loss.item())
+            # print(loss.item())
+            f, (p, r) = f1_score(output, targets.float())
+            f1.update(f)
+            prec.update(p)
+            rec.update(r)
+
             # slightly smooth output
             output = torch.softmax(output, dim=1)
             pred = output.max(1, keepdim=True)[1].float()
@@ -88,11 +91,6 @@ def validate(val_loader, model, criterion, log_every=1):
             print(F1_measure)
             # store various accuracy measures
             acc.update(pred.eq(targets.view_as(pred)).float().mean().item(), targets.size(0))
-            f, (p, r) = f1_score(output, targets)
-
-            f1.update(f)
-            prec.update(p)
-            rec.update(r)
 
             # measure elapsed time
             batch_time.update(time.time() - end)
@@ -115,8 +113,8 @@ def validate(val_loader, model, criterion, log_every=1):
 
 
 def f1_score(output, target, epsilon=1e-7):
-    # output had softmax already applied
-    probas = (output[:, 1, :, :] > .5).float()
+    # turn output into 0-1 map
+    probas = (output[:, 1, :, :] > 0.).float()
 
     TP = (probas * target).sum(dim=1)
     precision = TP / (probas.sum(dim=1) + epsilon)
@@ -125,28 +123,21 @@ def f1_score(output, target, epsilon=1e-7):
     f1 = f1.clamp(min=epsilon, max=1-epsilon)
     return f1.mean().item(), (precision.mean().item(), recall.mean().item())
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--count-params", default=False, type=bool)
-args = parser.parse_args()
 
 cc = Configs()
 print("Loading stored model")
 model = SegnetConvLSTM(cc.hidden_dims, decoder_out_channels=2, lstm_nlayers=len(cc.hidden_dims),
                        vgg_decoder_config=cc.decoder_config)
-tu.load_model_checkpoint(model, '../train-results/latest_model.torch', inference=False, map_location=device)
+tu.load_model_checkpoint(model, '../train-results/model-fixed.torch', inference=False, map_location=device)
 model.to(device)
-if args.count_params:
-    counter = 0
-    for p in model.parameters():
-        counter += 1
-    print(f"Model loaded contains {counter} parameters")
 print("Model loaded")
 
-tu_test_dataset = TUSimpleDataset(config.tr_root, config.tr_subdirs, config.tr_flabels, shuffle=False)#, shuffle_seed=9)
+tu_test_dataset = TUSimpleDataset(config.ts_root, config.ts_subdirs, config.ts_flabels, shuffle=False)#, shuffle_seed=9)
 
 # build data loader
 tu_test_dataloader = DataLoader(tu_test_dataset, batch_size=cc.test_batch, shuffle=True, num_workers=2)
 
-criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor([0.02, 1.02])).to(device) # using crossentropy for weighted loss
+# using crossentropy for weighted loss
+criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor([0.02, 1.02])).to(device)
 
 validate(tu_test_dataloader, model, criterion, log_every=1)

train.py
@@ -82,58 +82,6 @@ def train(train_loader:DataLoader, model:SegnetConvLSTM, criterion, optimizer, e
     return losses.avg, acc.avg, f1.avg
 
 
-def validate(val_loader, model, criterion, log_every=1):
-    batch_time = AverageMeter('Time', ':6.3f')
-    losses = AverageMeter('Loss', ':.4e')
-    acc = AverageMeter('Acc', ':6.4f')
-    f1 = AverageMeter('F1', ':6.4f')
-    prec = AverageMeter('Prec', ':6.4f')
-    rec = AverageMeter('Recall', ':6.4f')
-    progress = ProgressMeter(
-        len(val_loader),
-        [batch_time, losses, acc, f1, prec, rec],
-        prefix='Test: ')
-
-    # switch to evaluate mode
-    model.eval()
-
-    with torch.no_grad():
-        end = time.time()
-        for batch_no, (list_batched_samples, batched_targets) in enumerate(val_loader):
-            # move data to gpu (or cpu if device is unavailable)
-            list_batched_samples = [t.to(device) for t in list_batched_samples]
-            batched_targets = batched_targets.to(device)
-
-            # compute output
-            output = model(list_batched_samples)
-
-            loss = criterion(output, batched_targets)
-
-            losses.update(loss.item(), batched_targets.size(0))
-
-            # store various accuracy measures
-            acc.update(pixel_accuracy(output, batched_targets), batched_targets.size(0))
-            f, (p, r) = f1_score(output, batched_targets)
-
-            f1.update(f)
-            prec.update(p)
-            rec.update(r)
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-
-            if batch_no % log_every == 0:
-                progress.display(batch_no)
-
-        # TODO: this should also be done with the ProgressMeter
-        # print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-        #       .format(top1=top1, top5=top5))
-
-        # currently returning loss instead of accuracy
-        return losses.avg
-
-
 def IoU_accuracy(prediction, target):
     """
         Computes the intersection over union accuracy; this measure
@@ -214,7 +162,6 @@ model = SegnetConvLSTM(hidden_dims, decoder_out_channels=2, lstm_nlayers=len(hid
 if cc.load_model:
     trainu.load_model_checkpoint(model, '../train-results/model.torch', inference=False, map_location=device)
 
-
 model.to(device)
 
 # define loss function (criterion) and optimizer
@@ -229,18 +176,17 @@ losses_ = []
 accs = []
 f1s = []
 test_losses = []
-# optimizer.zero_grad()
 for epoch in range(epochs):
-    #adjust_learning_rate(optimizer, epoch, init_lr)
+    # adjust_learning_rate(optimizer, epoch, init_lr)
 
     # do one train step
     loss_val, a, f = train(tu_train_dataloader, model, criterion, optimizer, epoch, log_every=16)
     losses_.append(loss_val)
     accs.append(a)
     f1s.append(f)
-    # evaluate model performance
-    #loss_eval_val = validate(tu_test_dataloader, model, criterion, log_every=24)
-    #test_losses.append(loss_eval_val)
+
+    # did not evaluate model performance on eval set during training since
+    # it's already expensive enough as it is
 
     # save model at each epoch (overwrite model because of memory usage)
     trainu.save_model_checkpoint(model, 'model.torch', epoch=epoch)

utils/data_utils.py
@@ -9,6 +9,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from utils.cuda_device import device
+import utils.config as config
 import torchvision.transforms.functional as F
 import torch
 
@@ -104,8 +105,8 @@ class TUSimpleDataset(Dataset):
         image = np.zeros(new_dim).astype(np.float32)
         c = (1., 1., 1.)   # white lane points (normalized)
         # c = (255, 255, 255)
-        scale_factorX = new_dim[0] * (1./original_shape[0])
-        scale_factorY = new_dim[1] * (1./original_shape[1])
+        scale_factorY = new_dim[0] * (1./original_shape[0])
+        scale_factorX = new_dim[1] * (1./original_shape[1])
         for lane in gt_lanes_vis:
             # rescale lane points
             lane = [(x*scale_factorX, y*scale_factorY) for (x, y) in lane]
@@ -134,11 +135,8 @@ class TUSimpleDataset(Dataset):
         # torch image: C X H X W
         return torch.from_numpy(image.transpose((2, 0, 1)))
 
-# :param eval_seed: Evaluation set is chosen among the many samples by means of a randomly init bit-mask,
-#                           so that no bias comes from data ordering.
 
-
-def show_plain_images(images, n_frames):
+def show_plain_images(images, n_frames, save=False, fname=None):
     plt.figure(num=None, figsize=(20, 4), dpi=80)
     for i in range(n_frames):
         ax = plt.subplot(1, n_frames, i + 1)
@@ -151,10 +149,15 @@ def show_plain_images(images, n_frames):
             ax.imshow(images[i].squeeze().numpy(), cmap='gray')
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
-    plt.show()
+    if save and fname:
+        plt.savefig(os.path.join(config.data_dir, fname), dpi=200)
+    else:
+        plt.show()
+    # plt.close()
 
 
-def show_annotated_image(gt_lanes:list, gt_hsamples:list, gt_frame_filename:str=None):
+
+def show_annotated_image(gt_lanes:list, gt_hsamples:list, gt_frame_filename:str=None, resize=True, save=False, filename=''):
     """
     Utility method for showing an annotated frame (target).
     For information on the annotation structure please refer to
@@ -165,34 +168,58 @@ def show_annotated_image(gt_lanes:list, gt_hsamples:list, gt_frame_filename:str=
     :return:
     """
     gt_lanes_vis = [[(x, y) for (x, y) in zip(lane, gt_hsamples) if x >= 0] for lane in gt_lanes]
+    if resize:
+        imageb = np.zeros((128, 256, 3))
+    else:
+        imageb = np.zeros((720, 1280, 3))
+
     if gt_frame_filename:
         image = cv2.imread(gt_frame_filename)
+        if resize:
+            image = cv2.resize(image, (256, 128))
         c = (0, 255, 0)
-    else:
-        c = (255, 255, 255)
-        image = np.zeros((720, 1280))
 
+    c = (255, 0, 0)
+
+
+    scale_factorX = 128 * (1. / 720)
+    scale_factorY = 256 * (1. / 1280)
     for lane in gt_lanes_vis:
-        cv2.polylines(image, np.int32([lane]), isClosed=False, color=c, thickness=5)
-    # normalize image/255
-    plt.imshow(image, cmap='gray')
+        if resize:
+            lane = [(x * scale_factorY, y * scale_factorX) for (x, y) in lane]
+        cv2.polylines(imageb, np.int32([lane]), isClosed=False, color=c, thickness=5)
+        # print(imageb.shape)
+    if resize:
+        # imageb = cv2.resize(imageb, (256, 128))
+        plt.imshow(imageb.astype(np.int32))
+        plt.show()
+        # uint8 addition uses modulo (230+30=5)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    imageb = imageb + image
+    plt.imshow(imageb.astype(np.int32))
+    if save:
+        plt.savefig(filename, dpi=200)
     plt.show()
-    return image
+    # plt.imshow(image, cmap='gray')
+    # plt.show()
+    return imageb
 
 
 # original values: 3626 train sample - 2782 test samples
 if __name__ == '__main__':
-    # labels = [json.loads(line) for line in open('/Users/nick/Desktop/train_set/label_data_0313.json', 'r')]
-    # lanes = labels[0]['lanes']
-    # height = labels[0]['h_samples']
-    # frame = os.path.join('/Users/nick/Desktop/train_set', labels[0]['raw_file'])
-    # show_annotated_image(lanes, height)
+    labels = [json.loads(line) for line in open('/Users/nick/Desktop/train_set/label_data_0531.json', 'r')]
+    sample_no = 299
+    for sample_no in range(0, 100, 10):
+        lanes = labels[sample_no]['lanes']
+        height = labels[sample_no]['h_samples']
+        frame = os.path.join('/Users/nick/Desktop/train_set', labels[sample_no]['raw_file'])
+        show_annotated_image(lanes, height, frame, resize=False, save=True, filename=f'image_{sample_no}.png')
     root = '/Users/nick/Desktop/train_set/clips/'
     subdirs = ['0601', '0531', '0313-1', '0313-2']
     flabels = ['/Users/nick/Desktop/train_set/label_data_0601.json',
                '/Users/nick/Desktop/train_set/label_data_0531.json',
                '/Users/nick/Desktop/train_set/label_data_0313.json']
-    # toTensor will make sure channel ordering is 'pytorch-style' image = image.transpose((2, 0, 1))
 
     data_transform = transforms.Compose([
         transforms.Resize((128, 256)),
@@ -220,7 +247,7 @@ if __name__ == '__main__':
 
     for i, (batched_samples, batched_target) in enumerate(tu_dataloader):
         # print(len(batched_samples))
-        print("TARGET:",torch.max(batched_target), torch.sum(batched_target))
+        print("TARGET:", torch.max(batched_target), torch.sum(batched_target))
         # print(batched_samples[0].size(), batched_target.size())
         for i in range(2):
             samples = []
@@ -232,7 +259,7 @@ if __name__ == '__main__':
                     samples.append(b.squeeze())
                 print("samples info")
                 print(a.squeeze().size())
-                print(torch.max(a), torch.mean(a), torch.sum(a))
+                print(torch.max(a).item(), torch.mean(a).item(), torch.sum(a).item())
 
             print("Single target shape:", batched_target[i].size())
             show_plain_images(samples + [batched_target[i]], len(samples) + 1)

visual_evaluation.py
@@ -10,21 +10,24 @@ from utils.cuda_device import device
 from utils.data_utils import TUSimpleDataset
 from utils.data_utils import DataLoader
 from utils.data_utils import show_plain_images
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
-
+    # load model
     cc = Configs()
     print("Loading stored model")
     model = SegnetConvLSTM(cc.hidden_dims, decoder_out_channels=2, lstm_nlayers=len(cc.hidden_dims), vgg_decoder_config=cc.decoder_config)
-    tu.load_model_checkpoint(model, '/Volumes/Samsung128/projects/ispr-project/train-results/model.torch', inference=False, map_location=device)
-    # print("Model with {} parameters correctly loaded", len(model.parameters()))
+    tu.load_model_checkpoint(model, '/Volumes/Samsung128/projects/ispr-project/train-results/model-fixed.torch', inference=False, map_location=device)
     print("Model loaded")
+    # create dataloader
     tu_test_dataset = TUSimpleDataset(config.ts_root, config.ts_subdirs, config.ts_flabels)
     tu_dataloader = DataLoader(tu_test_dataset, batch_size=2, shuffle=True)
     model.train()
     with torch.no_grad():
 
-        for i, (frames, targets) in enumerate(tu_dataloader):
+        for batchno, (frames, targets) in enumerate(tu_dataloader):
             output = model(frames)
             targets_ = targets.squeeze(1).long()
 
@@ -46,4 +49,9 @@ if __name__ == '__main__':
                     # print(torch.max(a), torch.mean(a), torch.sum(a))
                 # print("Single target shape:", targets[i].size())
                 # print("Single output shape:", output[i][1, :, :].unsqueeze(0).size())
-                show_plain_images(samples + [targets[i]] + [output[i].unsqueeze(0)], len(samples) + 2)
+                # o = cv2.addWeighted(samples[-1].permute(1, 2, 0).numpy(), 1., targets[i].permute(1, 2, 0).numpy().repeat(3, axis=2), 1, 0)
+                o = samples[0].permute(1, 2, 0).numpy() + targets[i].permute(1, 2, 0).numpy()
+                # plt.imshow(o.astype(np.float32))
+                # plt.show()
+                show_plain_images(samples + [targets[i]] + [output[i].unsqueeze(0)], len(samples) + 2, save=True,
+                                  fname=f'visual_{batchno}-{i}.png')